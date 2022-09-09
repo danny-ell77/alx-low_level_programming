233940db0b1de9a78516726ada7b0077d85951dd
Daniel Olah <danielolahskybrow77@gmail.com>
2022-09-09 04:55:10 -0700

    #5 done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Function that prints 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+
+	return (0);
+}
