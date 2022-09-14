a5532ac5537585710e399340ad3648c63cf737d7
Daniel Olah <danielolahskybrow77@gmail.com>
2022-09-13 19:52:27 -0700

    #2 done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,14 @@
  * lowercase using only `_putchar`
  * Return: 0
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	char c = 'a';
-	int i = 0
+	char c;
+	int i = 0;
 
 	while (i <= 10)
 	{
+		c = 'a';
 		while (c <= 'z')
 		{
 			_putchar(c);
