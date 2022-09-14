4494db5f018605a451827a22b2670f39b3727b21
Daniel Olah <danielolahskybrow77@gmail.com>
2022-09-13 19:39:18 -0700

    #1 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Prints "_putchar" followed by new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alx[] = "_putchar";
+	int i  = 0;
+
+	while (alx[i] != '\0')
+	{
+		_putchar(alx[i]);
+		i++
+	}
+
+	_putchar("\n");
+
+	return (0);
+}
