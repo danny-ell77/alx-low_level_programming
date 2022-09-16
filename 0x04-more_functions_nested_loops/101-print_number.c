f3f74e9f6e033eccedf972511f616fb32a951074
Daniel Olah <danielolahskybrow77@gmail.com>
2022-09-15 17:20:32 -0700

    advanced tasks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,41 +7,18 @@
  */
 void print_number(int n)
 {
-	char num[1000];
-	unsigned int z;
-	int m, b;
-
-	b = 10;
-
-	if (n < 10 && n >= 0)
+	// If number is smaller than 0, put a - sign
+    	// and change number to positive
+	if (n < 0)
 	{
-		_putchar (n + '0');
+		putchar('-');
+		n = -n;
 	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
-	}
-
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+ 
+    // Remove the last digit and recur
+    if (n/10)
+        print(n/10);
+ 
+    // Print the last digit
+    putchar(n%10 + '0');
 }
